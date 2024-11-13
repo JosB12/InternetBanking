9073aec3409b47d08f4a0c5f27544a436d9882e3
Jordysvp <Jordysvp@gmail.com>
2024-11-12 23:19:57 -0400

    logica de usuarios lista, solo falta gestion de productos

--- InternetBanking.Core.Application/ViewModels/User/EditProfileViewModel.cs
+++ InternetBanking.Core.Application/ViewModels/User/EditProfileViewModel.cs
@@ -10,8 +10,8 @@ namespace InternetBanking.Core.Application.ViewModels.User
         public string Cedula { get; set; }
         public string Email { get; set; }
         public string UserName { get; set; }
-        public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
         public decimal? MontoAdicional { get; set; } // Solo visible si el usuario es Cliente
         public TipoUsuario TipoUsuario { get; set; }
     }
